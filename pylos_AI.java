388cf361231c650686a2d20186b8a00a6b4a6fe5
Ev_01 <evgenyrogozik@gmail.com>
2016-05-02 20:26:17 +0800

    variables are added, function drawBoard is almost complete

--- pylos_AI.java
+++ pylos_AI.java
@@ -10,15 +10,78 @@ import java.io.*;
 public class pylos_AI {
 
 	//global variables here
+	int white_balls = 15;
+	int black_balls = 15;
 
-	public int game_loop () { 
-		//TODO here
+	int playertype = 0;
+
+	String moveType = "add";
+
+	String color = "white";
+	int position = 20;
+
+	boolean win = false;
+
+	int[][] layer_1 = new int[4][4];
+	int[][] layer_2 = new int[3][3];
+	int[][] layer_3 = new int[2][2];
+	int layer_4 = 0;
+
+	long duration = 0; //The timing of the AI's move
+
+	public static void main(String[] args) {
+		pylos_AI instance = new pylos_AI();
+		instance.drawBoard();
+		/*
+			while(win == false) {
+				drawBoard();
+				makeMove();
+				checkwin();
+			}
+		 */	
+	}
+
+	public void drawBoard() {
+		System.out.println("==========================");
+		System.out.println("Current game board");
+		System.out.println("==========================");
+
+		String newLine = System.getProperty("line.separator");//This will retrieve line separator dependent on OS.
+
+		System.out.println(newLine);
+		System.out.println("Move: {type: " + moveType + ", position: " + position + ", color: " + color + "}");
+		System.out.println("Time: " + duration);
+		System.out.println();
+
+		for (int i = 0; i<4; i++) {
+			for (int j = 0; j<4; j++) {
+				if (layer_1[i][j] == 1) System.out.print("w ");
+				else if (layer_1[i][j] == 2) System.out.print("b ");
+				else System.out.print("o ");
+
+				System.out.print("    ");
+
+				if (i<3 && j<3) {
+					if (layer_2[i][j] == 1) System.out.print("w ");
+					else if (layer_2[i][j] == 2) System.out.print("b ");
+					else System.out.print("o ");
+				}
+
+				if (i<2 && j<2) {
+					if (layer_3[i][j] == 1) System.out.print("w ");
+					else if (layer_3[i][j] == 2) System.out.print("b ");
+					else System.out.print("o ");
+				}
+			}
+		}
+
 	}
 
 	public int minimax(int alpha, int beta, int maxDepth, int playertype) { 
 		long startTime = System.nanoTime();
 		//TODO here
 		long endTime = System.nanoTime();
-		long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+		duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+		return 0;
 	}
 }
