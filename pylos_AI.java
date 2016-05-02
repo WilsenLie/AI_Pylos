cb31a1d006de65b5da5107bf946a03817efe962c
Ev_01 <evgenyrogozik@gmail.com>
2016-05-02 17:57:46 +0800

    pylos_AI.java file created

---
+++ pylos_AI.java
@@ -0,0 +1,24 @@
+/*
+ *pylos_AI.java
+ *@author Evgeny Rogozik and Wilsen Lie
+ *This is the implementation of Minimax (or Negamax may be) algorithm with alpha beta pruning
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class pylos_AI {
+
+	//global variables here
+
+	public int game_loop () { 
+		//TODO here
+	}
+
+	public int minimax(int alpha, int beta, int maxDepth, int playertype) { 
+		long startTime = System.nanoTime();
+		//TODO here
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+	}
+}
